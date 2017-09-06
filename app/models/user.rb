3ce30c2a7dc1ff7079bde258f8ae7d4fd6a03374
Monica Kosciuk <koscim@Monicas-MacBook-Pro.local>
2017-09-06 11:44:19 -0400

    Create and implement feature tests for sign up, sign in, and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
-  validates :email, presence: true,
+  validates :email,
     format: { with: /\A.+@.+\..+\z/i }
-  validates :encrypted_password, presence: true,
+  validates :encrypted_password,
     length: { minimum: 6 }
   validates :sign_in_count, presence: true,
     numericality: { greater_than_or_equal_to: 0, only_integer: true }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,5 +24,26 @@ RSpec.describe User, type: :model do
 
   it { should have_valid(:updated_at).when(Date.parse('11/11/2017')) }
 
+  it 'has a matching password confirmation for the password' do
+    user = User.new
+    user.first_name = 'Rodger'
+    user.last_name = 'Grouch'
+    user.username = 'schmoopers104'
+    user.email = 'joeshmoe@gmail.com'
+    user.password = 'password'
+    user.password_confirmation = 'password'
+
+    expect(user).to be_valid
+    expect(user.errors[:password_confirmation]).to be_blank
+  end
+
+  it 'gives an error if the password confirmation does not match' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 
 end
