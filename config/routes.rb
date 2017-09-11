93cf0e7e577761fe94ede646fc1b875a4a58da0c
goodcleanfun <robertfbatesjr@gmail.com>
2017-09-11 11:28:18 -0400

    Add tests for user updates review and user deletes review

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'art_labels#index'
 
   resources :art_labels do
-    resources :reviews, only: [:index, :new, :create, :show, :destroy]
+    resources :reviews
   end
 
 
---
+++ spec/features/user_deletes_review_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'user deletes their created review', %Q{
+  As an authorized user
+  I want to view my review for a given art label
+  So that I can remove that review
+} do
+
+  # ACCEPTANCE CRITERIA
+  # * If I am an authorized user, I can see a delete button for my review
+  #   Delete button deletes the user's review if and
+  #   only if the user created the review
+
+  scenario 'authorized user can delete their review' do
+    user = FactoryGirl.create(:user)
+    review = FactoryGirl.create(:review, user: user)
+    art_label = review.art_label
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    click_link review.art_label.name
+    expect(page).to have_content(art_label.name)
+    expect(page).to have_content(review.feels)
+
+    click_button 'Delete'
+
+    expect(page).to have_content(art_label.name)
+    expect(page).to_not have_content(review.feels)
+  end
+end
