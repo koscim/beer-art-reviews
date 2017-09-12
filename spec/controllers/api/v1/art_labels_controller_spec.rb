22cd91da215105f5a23285e6045e1fe60f95c28d
Monica Kosciuk <koscim@Monicas-MacBook-Pro.local>
2017-09-12 13:28:33 -0400

    change non-react index and show page tests to pending

--- spec/controllers/api/v1/art_labels_controller_spec.rb
+++ spec/controllers/api/v1/art_labels_controller_spec.rb
@@ -12,10 +12,9 @@ RSpec.describe Api::V1::ArtLabelsController, type: :controller do
       expect(response.content_type).to eq("application/json")
 
       expect(returned_json.length).to eq 1
-
       expect(returned_json["art_labels"][0]["name"]).to eq art_label.name
       expect(returned_json["art_labels"][0]["brewery"]).to eq art_label.brewery
-      expect(returned_json["art_labels"][0]["label_photo"]).to eq art_label.label_photo
+      expect(returned_json["art_labels"][0]["label_photo"]["url"]).to eq art_label.label_photo.url
       expect(returned_json["art_labels"][0]["beer_style"]).to eq art_label.beer_style
       expect(returned_json["art_labels"][0]["art_style"]).to eq art_label.art_style
       expect(returned_json["art_labels"][0]["container_type"]).to eq art_label.container_type
